8d3f01370c6a5160e3e634b33dc4c9ed682b152e
GeoFyl <98826499+GeoFyl@users.noreply.github.com>
2025-01-25 11:52:31 +0000

    Created player class

---
+++ Source/BubbleBunnyBout/PlayerClass.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerClass.h"
+
+// Sets default values
+APlayerClass::APlayerClass()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	leftArm = CreateDefaultSubobject<USceneComponent>("LeftArm");
+	rightArm = CreateDefaultSubobject<USceneComponent>("RightArm");
+}
+
+// Called when the game starts or when spawned
+void APlayerClass::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerClass::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerClass::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
