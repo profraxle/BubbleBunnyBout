9d1de6824ca3f43d8c63e8225f80bc4f15b24295
AbertayFraxle <2100629@uad.ac.uk>
2025-01-25 23:21:16 +0000

    Revert "Merge branch 'master' of https://github.com/AbertayFraxle/BubbleBunnyBout"
    This reverts commit 1c81629196436de5b1ace27303b5eb488eb84d32, reversing
    changes made to 1b3a1bedb30ddefb55a98bb7c44d95c25b956754.

--- Source/BubbleBunnyBout/PlayerClass.h
+++ Source/BubbleBunnyBout/PlayerClass.h
@@ -8,7 +8,6 @@
 #include "BoutCentre.h"
 #include "Kismet/KismetMathLibrary.h"
 #include "Kismet/GameplayStatics.h"
-#include "Engine/LocalPlayer.h"
 #include "PlayerClass.generated.h"
 
 UCLASS()
@@ -39,10 +38,6 @@ public:
 	void RotateRightArm(const FInputActionValue& Value);
 	void RaiseLeftArm(const FInputActionValue& Value);
 	void RaiseRightArm(const FInputActionValue& Value);
-
-	UFUNCTION(BlueprintCallable)
-	void Die();
-
 
 	//Movement Functions
 	void Move(const FInputActionValue& Value);
@@ -110,12 +105,4 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UInputAction* rightArmRaiseIA;
 
-	// Movement Constraint
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float moveAngleConstraint = 40;
-
-	// Death
-	int playerID;
-	bool dying = false;
-	float deathTimeElapsed = 0.f;
 };
